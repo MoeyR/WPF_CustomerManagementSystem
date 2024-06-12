42c3f1b844bf4137e54e46b274cd4e83c3674780
MoeyR <ranm@sheridancollege.ca>
2024-06-11 23:02:26 -0400

    Add project files.

---
+++ WPF_CMS/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPF_CMS.Models;
+using WPF_CMS.ViewModels;
+
+namespace WPF_CMS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MainViewModel _viewModel;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _viewModel = new MainViewModel();
+
+            _viewModel.LoadCustomers();
+
+            DataContext = _viewModel;
+        }
+
+        private void ClearSelectedCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            _viewModel.ClearSelectedCustomer();
+        }
+
+        private void SaveCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            try 
+            { 
+                string name = NameTextBox.Text.Trim();
+                string idNumber = IdTextBox.Text.Trim();
+                string address = AddressTextBox.Text.Trim();
+
+                _viewModel.SaveCustomer(name, idNumber, address);
+            } 
+            catch(Exception ex) 
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void AddAppointment_Click(object sender, RoutedEventArgs e)
+        {
+            try 
+            { 
+                _viewModel.AddAppointment();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
---
+++ WPF_CMS/ViewModels/MainViewModel.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF_CMS.Models;
+
+namespace WPF_CMS.ViewModels
+{
+    public class MainViewModel: INotifyPropertyChanged
+    {
+        //public List<Customer> Customers { get; set; } = new();
+        public ObservableCollection<CustomerViewModel> Customers { get; set; } = new();
+        public ObservableCollection<DateTime> Appointments { get; set; } = new();
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private DateTime? _selectedDate;
+        public DateTime? SelectedDate { get => _selectedDate; 
+            set 
+            {
+                if (_selectedDate != value)
+                {
+                    _selectedDate = value; 
+                    RaisePropertyChanged(nameof(SelectedDate));
+                }   
+            } 
+        }
+
+        private CustomerViewModel _selectedCustomer;
+
+        public CustomerViewModel SelectedCustomer
+        { 
+            get => _selectedCustomer;
+            set 
+            { 
+                if(value != _selectedCustomer)
+                {
+                    _selectedCustomer = value;
+                    RaisePropertyChanged(nameof(SelectedCustomer));
+                    LoadAppoitemnts(SelectedCustomer.Id);
+                }
+            } 
+        }
+
+        public void LoadCustomers()
+        {
+            Customers.Clear();
+            using (var db = new AppDbContext())
+            {
+                var customers = db.Customers.ToList();
+
+                foreach(var c in customers)
+                {
+                    Customers.Add(new CustomerViewModel(c));
+                }
+            }
+
+            
+        }
+
+        public void ClearSelectedCustomer()
+        {
+            _selectedCustomer = null;
+            RaisePropertyChanged(nameof(SelectedCustomer));
+        }
+
+        public void SaveCustomer(string name, string idNumber, string address)
+        {
+            if (SelectedCustomer != null)
+            {
+                //update customer
+                using (var db = new AppDbContext())
+                {
+                    var customer = db.Customers.Where(c => c.Id == SelectedCustomer.Id).FirstOrDefault();
+                    customer.Name = name;
+                    customer.IdNumber = idNumber;
+                    customer.Address = address;
+                }
+
+            }
+            else
+            {
+                //add customer
+                using (var db = new AppDbContext())
+                {
+                    var newCustomer = new Customer()
+                    {
+                        Name = name,
+                        IdNumber = idNumber,
+                        Address = address
+                    };
+                    db.Customers.Add(newCustomer);
+                    db.SaveChanges();
+                }
+                LoadCustomers();
+            }
+        }
+
+        public void LoadAppoitemnts(int customerId)
+        {
+            Appointments.Clear();
+            using (var db = new AppDbContext())
+            {
+                var appointments = db.Appointments.Where(a => a.CustomerId == customerId).ToList();
+
+                foreach(var a in appointments)
+                {
+                    Appointments.Add(a.Time);
+                }
+            }
+        }
+
+        public void AddAppointment()
+        {
+            if(SelectedCustomer == null)
+            {
+                return;
+            }
+
+            using(var db = new AppDbContext())
+            {
+                var newAppointment = new Appointment()
+                {
+                    Time = SelectedDate.Value,
+                    CustomerId = SelectedCustomer.Id
+                };
+                db.Appointments.Add(newAppointment);
+                db.SaveChanges();
+            }
+            SelectedDate = null;
+            LoadAppoitemnts(SelectedCustomer.Id);
+        }
+    }
+}
